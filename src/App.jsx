8f24eb6d06cc7d1d803cb76d7d5b75465b486147
Gregg <gregg.marayan@gmail.com>
2024-10-09 21:29:57 +0800

    Fixed Minor issues and made tailwind a bit efficient to read

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import BackGrid from "./components/BackGrid"
 import { BackgroundGradientAnimation } from "./components/background-gradient-animation"
 
 
 function App() {
 
-    return (
+  let ParaText = "bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20"
+  let mainDiv = "absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"
+
+  
+  return (
     <>
-      <BackgroundGradientAnimation>
-      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Welcome!
-        </p>
-      </div>
-    </BackgroundGradientAnimation>
+      <BackgroundGradientAnimation interactive = {false}>
+        <div className={mainDiv}>
+          <p className= {ParaText}>
+            Welcome!
+          </p>
+        </div>
+      </BackgroundGradientAnimation>
     </>
   )
 }
