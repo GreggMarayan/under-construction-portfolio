0daa3a682058da2f466ebdd2c6526fb1792c4690
Gregg <gregg.marayan@gmail.com>
2024-10-09 14:28:08 +0800

    Test Commit on MainSite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import BackGrid from "./components/BackGrid"
+import { BackgroundGradientAnimation } from "./components/background-gradient-animation"
 
 
 function App() {
 
     return (
     <>
-      <BackGrid text = "Under Construction" subtext = "Thank you for your patience!"/>
+      <BackgroundGradientAnimation>
+      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+          Welcome!
+        </p>
+      </div>
+    </BackgroundGradientAnimation>
     </>
   )
 }
