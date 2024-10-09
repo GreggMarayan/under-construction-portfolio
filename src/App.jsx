35101f1311f6f30666b9475d032983fc0521c0c5
Gregg <gregg.marayan@gmail.com>
2024-10-09 14:24:26 +0800

    MainSite First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BackGrid from "./components/BackGrid"
+
+
+function App() {
+
+    return (
+    <>
+      <BackGrid text = "Under Construction" subtext = "Thank you for your patience!"/>
+    </>
+  )
+}
+
+export default App
