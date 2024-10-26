076bbf04ba58ff3c0780d44beb297c8dcb8a9d9f
Gregg <gregg.marayan@gmail.com>
2024-10-26 17:36:02 +0800

    Added ShadCN init and few components (BackgroundLines does not work due to issues with dependencies: framer-motion)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import { BackgroundLines } from "@/components/ui/background-lines";
+import { Button, buttonVariants } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <>
+    <BackgroundLines>
+
+    <Button variant="ghost">Toggle Mode!</Button>
+    
+    </BackgroundLines>
+
     </>
   );
 }
