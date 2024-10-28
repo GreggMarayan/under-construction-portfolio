a81d4e047bd80071fe6815aca9b5f19bdd1c570f
Gregg <gregg.marayan@gmail.com>
2024-10-28 19:56:18 +0800

    fixed dependency issues with aceternity and shadcn (React dom issue rolled back from 19.x to 18.3 for compatibility.) also added themes dependency, soon to add darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,39 @@
+import { BackgroundLines } from "@/components/ui/background-lines";
 import { Button, buttonVariants } from "@/components/ui/button"
 import { Header } from "@/components/ui/Header"
+import { useState } from "react";
 
 export default function Home() {
+  // const [mode, setMode] = useState()   
+  function handleDark(){
+
+  }
+
   return (
     <>
-      <Header/>
+      <header className="flex justify-between justify-sticky items-center sticky top-0 bg-white shadow-md z-10">
+        <div>
+          <h1 className="text-xl font-bold p-6">Gregg Marayan</h1>
+        </div>
+        <div>
+          <nav>
+            <ul className="flex space-x-4 pr-10">
+              <li><Button variant="ghost">Dark mode?</Button></li>
+              <li><Button variant="ghost"><a href="#contact" className="text-gray-700">Contact</a></Button></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+
+        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          Sanjana Airlines, <br /> Sajana Textiles.
+        </h2>
+        <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+          Get the best advices from our experts, including expert artists,
+          painters, marathon enthusiasts and RDX, totally free.
+        </p>
+      </BackgroundLines>
     </>
   );
 }
--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -4,12 +4,12 @@ export function Header() {
         <>
             <header className="flex justify-between justify-sticky items-center sticky top-0 bg-white shadow-md z-10">
                 <div>
-                    <h1 className="text-xl font-bold p-10">Felhony!!!</h1>
+                    <h1 className="text-xl font-bold p-10">Gregg Marayan</h1>
                 </div>
                 <div>
                     <nav>
-                        <ul className="flex space-x-4 pr-10">
-                            <li><a href="#home" className="text-gray-700">Music</a></li>
+                        <ul className="flex space-x-4 p-10">
+                            <li><a href="#home" className="text-white-700">Music</a></li>
                             <li><a href="#about" className="text-gray-700">Tours</a></li>
                             <li><a href="#contact" className="text-gray-700">Contact</a></li>
                         </ul>
